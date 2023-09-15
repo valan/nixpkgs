ad11101d52667265980075fa8ab34fa86ee2d186
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-09-15 14:07:17 +0200

    python3Packages.traitsui: 7.4.3 -> 8.0.0

--- pkgs/development/python-modules/traitsui/default.nix
+++ pkgs/development/python-modules/traitsui/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "traitsui";
-  version = "7.4.3";
+  version = "8.0.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-2zJRfpYioFgMIyCeE/gDLOoA5hxndJNnCf9F52M11bk=";
+    hash = "sha256-kBudHLxFUT4Apzl2d7CYRBsod0tojzChWbrUgBv0A2Q=";
   };
 
   propagatedBuildInputs = [
