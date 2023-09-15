9bee3d377ac09fd9ed116919bcc72c57f15533e0
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-09-15 14:07:02 +0200

    python3Packages.reproject: 0.10.0 -> 0.12.0

--- pkgs/development/python-modules/reproject/default.nix
+++ pkgs/development/python-modules/reproject/default.nix
@@ -16,14 +16,14 @@
 
 buildPythonPackage rec {
   pname = "reproject";
-  version = "0.10.0";
+  version = "0.12.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.8";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-OKxPPKcVVrEVUGR8Zaphn7ur9HOuqQKa9gnMo2RQQME=";
+    hash = "sha256-jb4efKT5jMY0ECV+ab5rpUHEk+tT4T2MioCRxs92TbI=";
   };
 
   postPatch = ''
