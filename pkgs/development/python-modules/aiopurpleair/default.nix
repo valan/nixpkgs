f4d1377ca65856e463342dc8d784ce9a96dcd758
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-09-15 14:05:43 +0200

    python3Packages.aiopurpleair: 2022.12.1 -> 2023.08.0

--- pkgs/development/python-modules/aiopurpleair/default.nix
+++ pkgs/development/python-modules/aiopurpleair/default.nix
@@ -14,7 +14,7 @@
 
 buildPythonPackage rec {
   pname = "aiopurpleair";
-  version = "2022.12.1";
+  version = "2023.08.0";
   format = "pyproject";
 
   disabled = pythonOlder "3.9";
@@ -23,7 +23,7 @@ buildPythonPackage rec {
     owner = "bachya";
     repo = pname;
     rev = "refs/tags/${version}";
-    hash = "sha256-YmJH4brWkTpgzyHwu9UnIWrY5qlDCmMtvF+KxQFXwfk=";
+    hash = "sha256-5aWxJXCBXXc3JPDC8xhw4JhowTcFAAtjmKOY1BeyGW8=";
   };
 
   patches = [
