c6af03e05ac1ee46a7ef882f6020984748e70a6b
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-09-15 14:06:47 +0200

    python3Packages.pyface: 7.4.4 -> 8.0.0

--- pkgs/development/python-modules/pyface/default.nix
+++ pkgs/development/python-modules/pyface/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "pyface";
-  version = "7.4.4";
+  version = "8.0.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.6";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-0F+qxFOUvVWYWQahIM8AEv58rCxmNJYsBTC2pjUO2yI=";
+    hash = "sha256-fhNhg0e3pkjtIM29T9GlFkj1AQKR815OD/G/cKcgy/g=";
   };
 
   propagatedBuildInputs = [
